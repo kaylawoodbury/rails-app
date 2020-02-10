685990c50af43943f160c78e202106d7c58ec5be
Anish2504 <anish.kanswal@gmail.com>
2020-02-10 12:49:00 +0100

    Turned off generators

--- config/application.rb
+++ config/application.rb
@@ -23,12 +23,19 @@ module RailsApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.generators do |generate|
+    generate.helper false
+		generate.assets false
+		generate.view_specs false
+		generate.helper_specs false
+		generate.routing_specs false
+    generate.controller_specs false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+  end
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
