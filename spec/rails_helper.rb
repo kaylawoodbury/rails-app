f1534e3bfed4e81a45e208789d8902248911c9c8
kaylawoodbury <kaylasandoval92@gmail.com>
2020-02-10 15:20:55 +0100

    create user can see articles tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,7 +64,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 end
 
-Shoulda::Matchers.comfigure do |config|
+Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
